ee03efe46e6315040234684ab7e82cdb464d54dc
Fabian Hiller <hillerfabian11@gmail.com>
2020-04-28 22:11:29 +0200

    Update CSS code for FileDetails component

--- src/components/core/media/FileDetails/FileDetails-styles.ts
+++ src/components/core/media/FileDetails/FileDetails-styles.ts
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 
-export const FileDetails = styled('div')({});
+export const FileDetails = styled('div')({
+  '& > .Detail + .Detail': {
+    marginTop: '8rem',
+  },
+});
 
 export const Image = styled('img')({
   width: '100%',
   height: 'auto',
+  borderRadius: '5rem',
   '& + .Detail': {
     marginTop: '40rem',
   },
