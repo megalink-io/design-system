4be272523e32d2f57a86996303c4818885680903
Fabian Hiller <hillerfabian11@gmail.com>
2020-04-28 22:13:22 +0200

    Update CSS code of AppBar component

--- src/components/core/navigation/AppBar/AppBar-styles.ts
+++ src/components/core/navigation/AppBar/AppBar-styles.ts
@@ -5,18 +5,13 @@ export const AppBar = styled('header')(({ theme }) => ({
   top: 0,
   left: 0,
   width: '100%',
-  height: '100%',
-  backgroundColor: theme.colors.black[1000],
-}));
-
-export const Content = styled('header')(({ theme }) => ({
-  width: '100%',
   height: '90rem',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'space-between',
   padding: '0 20rem',
   boxSizing: 'border-box',
+  backgroundColor: theme.colors.black[1000],
   '& > *:first-child': {
     flex: 1,
     overflow: 'hidden',
--- src/components/core/navigation/AppBar/AppBar-view.tsx
+++ src/components/core/navigation/AppBar/AppBar-view.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { ViewProps } from './AppBar-types';
-import { AppBar, Content } from './AppBar-styles';
+import { AppBar } from './AppBar-styles';
 
 /**
  * A top app bar that displays information and
  * actions related to the current page.
  * */
 export const ContainerView: React.FC<ViewProps> = React.memo(({ children }) => (
-  <AppBar className="AppBar">
-    <Content>{children}</Content>
-  </AppBar>
+  <AppBar className="AppBar">{children}</AppBar>
 ));
 
 export default ContainerView;
