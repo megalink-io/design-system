2e1254f0479ec398f5cb0508acf20e97a9c150b3
Fabian Hiller <hillerfabian11@gmail.com>
2020-05-31 19:43:56 +0200

    Update HTML and CSS of FileGallery component

--- src/components/core/media/FileGallery/FileGallery-styles.ts
+++ src/components/core/media/FileGallery/FileGallery-styles.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-export const FileGallery = styled('div')({
+export const FileGallery = styled('div')({});
+
+export const Items = styled('div')({
   display: 'flex',
   flexWrap: 'wrap',
   width: 'calc(100% + 50rem)',
--- src/components/core/media/FileGallery/FileGallery-view.tsx
+++ src/components/core/media/FileGallery/FileGallery-view.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ViewProps } from './FileGallery-types';
-import { FileGallery, ItemLink, ItemChild, Image } from './FileGallery-styles';
+import { FileGallery, Items, ItemLink, ItemChild, Image } from './FileGallery-styles';
 
 const FileGalleryView: React.FC<ViewProps> = ({
   rootPath,
@@ -9,23 +9,25 @@ const FileGalleryView: React.FC<ViewProps> = ({
   getSizeAttribute,
 }) => (
   <FileGallery className="FileGallery">
-    {files.map(file => (
-      <ItemLink key={file.id} to={`${rootPath}/${file.id}`} replace={deviceType === 'desktop'}>
-        <ItemChild>
-          {file.type.includes('image') && (
-            <Image
-              src={file.src}
-              srcSet={file.srcset
-                .sort((a, b) => (a.width > b.width ? 1 : -1))
-                .map(setFile => `${setFile.src} ${setFile.width}w`)
-                .concat([`${file.src} ${file.width}w`])
-                .join()}
-              sizes={getSizeAttribute(file.width, file.height)}
-            />
-          )}
-        </ItemChild>
-      </ItemLink>
-    ))}
+    <Items>
+      {files.map(file => (
+        <ItemLink key={file.id} to={`${rootPath}/${file.id}`} replace={deviceType === 'desktop'}>
+          <ItemChild>
+            {file.type.includes('image') && (
+              <Image
+                src={file.src}
+                srcSet={file.srcset
+                  .sort((a, b) => (a.width > b.width ? 1 : -1))
+                  .map(setFile => `${setFile.src} ${setFile.width}w`)
+                  .concat([`${file.src} ${file.width}w`])
+                  .join()}
+                sizes={getSizeAttribute(file.width, file.height)}
+              />
+            )}
+          </ItemChild>
+        </ItemLink>
+      ))}
+    </Items>
   </FileGallery>
 );
 
