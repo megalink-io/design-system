240927c1e08f86c95cd0f27ca273b6872a363578
Fabian Hiller <hillerfabian11@gmail.com>
2020-08-30 11:11:01 +0200

    Add Scrollability (layout) core component export

--- src/components/core/layout/index.ts
+++ src/components/core/layout/index.ts
@@ -2,6 +2,7 @@ export { default as Content } from './Content';
 export { default as Flexbox } from './Flexbox';
 export { default as Main } from './Main';
 export { default as Page } from './Page';
+export { default as Scrollability } from './Scrollability';
 export { default as Section } from './Section';
 export { default as Theme } from './Theme';
 export { default as Visibility } from './Visibility';
